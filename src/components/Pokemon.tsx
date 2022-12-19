a2edbda885c66006eb1cb291b9c2e767d764d301
ilathem <isaiahlathem@gmail.com>
2022-12-19 16:55:42 -0600

    test height change

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -127,8 +127,10 @@ const Pokemon: FunctionComponent<Props> = ({ pokemonName }) => {
 
   console.log(typeShown);
 
+  //max-h-[calc(100vh-12rem)]
+
   if (pokemon.data) return (
-    <motion.div className="absolute top-32 w-full max-h-[calc(100vh-12rem)] flex flex-col justify-start items-center border-2 border-purple-600">
+    <motion.div className="absolute top-32 w-full max-h-[calc(100vh-20rem)] flex flex-col justify-start items-center border-2 border-purple-600">
       <div className="overflow-y-scroll overflow-x-hidden h-full w-full flex flex-col items-center border-2 border-pink-400">
       <Image
         key="pokeImage"
